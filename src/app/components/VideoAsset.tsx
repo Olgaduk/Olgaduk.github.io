06032030f7e9f23e975ffad3781ebaef8ff263e8
Olga Duk <olga.duk@freshly.com>
2025-04-12 19:16:20 -0400

    Add video autoplay

--- src/app/components/VideoAsset.tsx
+++ src/app/components/VideoAsset.tsx
@@ -1,23 +1,37 @@
+import { useRef } from "react";
 import { Clip } from "../api/clips";
 
 export default function VideoAsset({ asset }: { asset: Clip }) {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
   const formatDuration = (duration: number) => {
     const minutes = Math.floor(duration / 60);
     const seconds = Math.floor(duration % 60);
     return `${minutes}:${seconds.toString().padStart(2, "0")}`;
   };
 
+  const handleMouseOver = () => {
+    videoRef.current?.play();
+  };
+
+  const handleMouseOut = () => {
+    videoRef.current?.pause();
+  };
+
   return (
-    <div className="relative aspect-square rounded-lg overflow-hidden group cursor-pointer">
+    <div
+      className="relative aspect-square rounded-lg overflow-hidden group cursor-pointer"
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
       <div className="relative w-full h-full">
-        {asset.assets.video && (
+        {asset.assets.previewVideo && (
           <video
+            ref={videoRef}
             src={asset.assets.previewVideo}
             className="w-full h-full object-cover"
             width={asset.width}
             height={asset.height}
-            muted
-            loop
           />
         )}
         {asset.duration && (
